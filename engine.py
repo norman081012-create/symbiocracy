d39d834f5e3f12af8111a4433e1889724abe708e
norman081012-create <norman081012@gmail.com>
2026-04-13 20:30:58 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -14,9 +14,6 @@ class Party:
         self.predict_ability = cfg['ABILITY_DEFAULT']; self.stealth_ability = cfg['ABILITY_DEFAULT']
         self.current_forecast = 0.0
         
-        # v3 新增投資池
-        self.invest_pools = {'build': 0.0, 'inv': 0.0, 'edu': 0.0, 'media': 0.0, 'pre': 0.0, 'stl': 0.0}
-        
         self.poll_history = {'小型': [], '中型': [], '大型': []}
         self.latest_poll = None
         self.poll_count = 0
@@ -26,12 +23,8 @@ class GameEngine:
     def __init__(self, cfg):
         self.year = 1
         self.party_A = Party(cfg['PARTY_A_NAME'], cfg); self.party_B = Party(cfg['PARTY_B_NAME'], cfg)
-        self.gdp = cfg['CURRENT_GDP']
-        
-        # v3 預算與跨年保留系統
-        self.budget_t = self.gdp * cfg['TAX_RATE']
-        self.pending_payouts = {'H': 0.0, 'R': 0.0, 'R_residual': 0.0}
-        
+        self.gdp = cfg['CURRENT_GDP']; self.total_budget = cfg['BASE_TOTAL_BUDGET'] + (self.gdp * cfg['HEALTH_MULTIPLIER'])
+        self.h_fund = cfg['H_FUND_DEFAULT']
         self.phase = 1; self.p1_step = 'draft_r' 
         self.p1_proposals = {'R': None, 'H': None}; self.p1_selected_plan = None
         self.ruling_party = self.party_A; self.r_role_party = self.party_A; self.h_role_party = self.party_B  
@@ -52,17 +45,19 @@ class GameEngine:
 
 def execute_poll(game, view_party, cost):
     view_party.wealth -= cost
-    error_margin = max(0.0, 15.0 - (view_party.predict_ability * 0.1) - (cost * 0.4))
+    error_margin = max(0.0, 15.0 - (view_party.predict_ability * 0.5) - (cost * 0.4))
     a_actual = game.party_A.support
     a_poll = max(0.0, min(100.0, a_actual + random.uniform(-error_margin, error_margin)))
     
     poll_type = '小型' if cost == 5 else '中型' if cost == 10 else '大型'
+    
     game.party_A.latest_poll = a_poll
     game.party_A.poll_history[poll_type].append(a_poll)
     
     b_poll = 100.0 - a_poll
     game.party_B.latest_poll = b_poll
     game.party_B.poll_history[poll_type].append(b_poll)
+    
     view_party.poll_count += 1
 
 def trigger_swap(game, penalty_amt, msg_prefix="政局動盪！"):
@@ -70,6 +65,6 @@ def trigger_swap(game, penalty_amt, msg_prefix="政局動盪！"):
     game.h_role_party, game.r_role_party = game.r_role_party, game.h_role_party
     game.swap_triggered_this_year = True
     game.emotion = min(100.0, game.emotion + 30.0) 
-    st.session_state.news_flash = f"🗞️ **【快訊】{msg_prefix}** 雙方被迫各捐款 {penalty_amt}，觸發換位！"
+    st.session_state.news_flash = f"🗞️ **【快訊】{msg_prefix}** 雙方被迫各強制捐款 {penalty_amt} 資金給第三政黨，觸發換位！"
     st.session_state.anim = 'snow'
     game.phase = 2
